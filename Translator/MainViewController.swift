c9ca718c34c45e7b1b9825181444093dda07a9b6
Hutr0 <Hutro-games@yandex.ru>
2021-12-03 00:06:08 +0300

    Row counter

--- Translator/MainViewController.swift
+++ Translator/MainViewController.swift
@@ -10,12 +10,14 @@ import Cocoa
 class MainViewController: NSViewController {
 
     @IBOutlet var languageFormat: NSTextView!
-    @IBOutlet var linesOfProgram: NSTextView!
+    @IBOutlet var rowsOfProgram: NSTextView!
     @IBOutlet var program: NSTextView!
     @IBOutlet var output: NSTextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        program.delegate = self
 
         let language = """
         Язык = "Begin" Звено...Звено Последнее Опер...Опер "End"
@@ -39,14 +41,59 @@ class MainViewController: NSViewController {
         """
         
         languageFormat.string = language
+        rowsOfProgram.string = "1"
     }
     
     @IBAction func clearButtonTapped(_ sender: Any) {
         program.string = ""
-        linesOfProgram.string = ""
+        rowsOfProgram.string = ""
     }
     
     @IBAction func executeButtonTapped(_ sender: Any) {
+    }
+}
+
+extension MainViewController: NSTextViewDelegate {
+    
+    func textDidChange(_ notification: Notification) {
+        let rowsCount = self.rowsOfProgram.string.components(separatedBy: "\n").count
+        let programRowsCount = self.program.string.components(separatedBy: "\n").count
+        
+        if rowsCount < programRowsCount {
+            addRows()
+        } else if rowsCount > programRowsCount {
+            deleteRows()
+        }
+    }
+    
+    private func addRows() {
+        guard let last = self.rowsOfProgram.string.components(separatedBy: "\n").last,
+              let intLast = Int(last)
+        else {
+            print("Error: Invalid last row element.")
+            return
+        }
+        
+        let num = intLast + 1
+        self.rowsOfProgram.string += "\n\(num)"
+    }
+    
+    private func deleteRows() {
+        guard let last = self.rowsOfProgram.string.components(separatedBy: "\n").last else {
+            print("Error: Invalid last row element.")
+            return
+        }
+        
+        for _ in 0...last.count {
+            self.rowsOfProgram.string.removeLast()
+        }
+        
+        let rowsCount = self.rowsOfProgram.string.components(separatedBy: "\n").count
+        let programRowsCount = self.program.string.components(separatedBy: "\n").count
+        
+        if rowsCount > programRowsCount {
+            deleteRows()
+        }
     }
 }
 
